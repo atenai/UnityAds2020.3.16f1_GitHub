31fb760fa34f6f58a0902a9ac189bfb26108e77e
atenai <alfamod47@yahoo.co.jp>
2024-01-28 16:24:12 +0900

    なんかエディタのやつ追加

---
+++ Assets/Scripts/UnityEditor拡張入門/NewBehaviourScript2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class NewBehaviourScript2 : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("name"));
+
+        //他、さまざまな処理
+        serializedObject.ApplyModifiedProperties();
+    }
+}
