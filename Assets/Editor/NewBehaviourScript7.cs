2b5cffd8d7a5125e2a552610b08ac51b2f62c848
atenai <alfamod47@yahoo.co.jp>
2024-02-09 19:32:41 +0900

    エディタ拡張

---
+++ Assets/Editor/NewBehaviourScript7.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+using UnityEngine.Events;
+
+public class NewBehaviourScript7 : EditorWindow
+{
+    [MenuItem("Kashiwabara/Example7")]
+    static void Open()
+    {
+        GetWindow<NewBehaviourScript7>();
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.ObjectField(null, typeof(object), false);
+
+        EditorGUILayout.ObjectField(null, typeof(Material), false);
+
+        EditorGUILayout.ObjectField(null, typeof(AudioClip), false);
+
+        var options = new[] { GUILayout.Width(64), GUILayout.Height(64) };
+
+        EditorGUILayout.ObjectField(null, typeof(Texture), false, options);
+
+        EditorGUILayout.ObjectField(null, typeof(Sprite), false, options);
+    }
+}
