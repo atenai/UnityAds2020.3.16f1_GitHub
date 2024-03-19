70e3d5421640229be621baa8634ca12c7b8bf76f
atenai <alfamod47@yahoo.co.jp>
2024-03-19 18:56:52 +0900

    数学

--- Assets/Scripts/Unityでわかるゲーム数学Scripts/Transform5_3.cs
+++ Assets/Scripts/Unityでわかるゲーム数学Scripts/Transform5_3.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Transform5_3 : MonoBehaviour {
+public class Transform5_3 : MonoBehaviour
+{
     Matrix4x4 matTransform;
     public Renderer rend;
     public Color colorCube = Color.red;
