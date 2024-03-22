6e625c5a6bbc03b501c94189b3d20c519fbdb102
atenai <alfamod47@yahoo.co.jp>
2024-03-22 15:04:47 +0900

    エディタ拡張

---
+++ Assets/Editor/ExampleWindow36.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+using UnityEngine.Events;
+
+public class ExampleWindow36
+{
+    [MenuItem("Kashiwabara/Example36", false, 1)]
+    static void Example()
+    {
+
+    }
+}
