7eaf696b16be90c24569e6648ad1b645c9ff3e20
atenai <alfamod47@yahoo.co.jp>
2024-03-26 15:05:46 +0900

    エディタ拡張

---
+++ Assets/Editor/ExampleWindow39.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+using UnityEngine.Events;
+
+public class ExampleWindow39
+{
+    //command(ctrl) + shift + k で実行
+    [MenuItem("Kashiwabara/Example39 %#k")]
+    static void Example()
+    {
+        Debug.Log("実行されました");
+    }
+}
