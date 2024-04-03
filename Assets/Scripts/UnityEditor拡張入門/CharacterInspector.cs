b0bbd4364ba629174bb12183345557a11c378b74
atenai <alfamod47@yahoo.co.jp>
2024-04-03 11:22:22 +0900

    エディタ拡張

--- Assets/Scripts/UnityEditor拡張入門/CharacterInspector.cs
+++ Assets/Scripts/UnityEditor拡張入門/CharacterInspector.cs
@@ -6,5 +6,19 @@ using UnityEditor;
 [CustomEditor(typeof(Character))]
 public class CharacterInspector : Editor
 {
+	Character character = null;
 
+	void OnEnable()
+	{
+		//Characterコンポーネントを取得
+		character = (Character)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();//元のGUIの要素をそのまま表示する
+
+		//攻撃力の数値をラベルとして表示する（インスペクターに要素を追加する）
+		EditorGUILayout.LabelField("攻撃力(エディタから追加)", character.攻撃力.ToString());
+	}
 }
