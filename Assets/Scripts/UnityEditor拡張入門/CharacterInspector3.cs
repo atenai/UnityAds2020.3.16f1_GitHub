3f94dbbd49ed170038ce31bfdfbe6f920e77193e
atenai <alfamod47@yahoo.co.jp>
2024-04-11 23:25:24 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/CharacterInspector3.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(Character3))]
+public class CharacterInspector3 : Editor
+{
+	Character3[] characters3;
+
+	void OnEnable()
+	{
+		characters3 = targets.Cast<Character3>().ToArray();
+	}
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUI.BeginChangeCheck();
+
+		//異なる値が2以上であればtrue
+		//Distinct()はLINQで重複した要素のみを削除する
+		EditorGUI.showMixedValue = characters3.Select(x => x.hp).Distinct().Count() > 1;
+
+		var hp = EditorGUILayout.IntSlider("Hp", characters3[0].hp, 0, 100);
+
+		EditorGUI.showMixedValue = false;
+
+		if (EditorGUI.EndChangeCheck())
+		{
+			//すべてのコンポーネントをUndoに登録
+			Undo.RecordObjects(characters3, "Change hp");
+
+			//すべてのコンポーネントに値を代入して更新
+			foreach (var character3 in characters3)
+			{
+				character3.hp = hp;
+			}
+		}
+	}
+}
