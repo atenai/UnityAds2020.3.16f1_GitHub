0056a4389f2bd971c71c310f8a303bc2795d2cd2
atenai <alfamod47@yahoo.co.jp>
2024-04-29 04:55:30 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU1_6_4_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU1_6_4_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+        A();
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+
+    void A()
+    {
+        Debug.Log("関数の呼び出し成功");
+    }
+}
