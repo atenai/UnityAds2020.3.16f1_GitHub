c251c25c82ae477cafea14024e57bc9b091612b7
atenai <alfamod47@yahoo.co.jp>
2024-05-05 12:49:17 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU1_6_6_2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU1_6_6_2 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        //float型の変数を宣言　変数名はx
+        float x;
+
+        //変数名xへ関数名Cの返り値データを割り当てる
+        x = C(5);
+
+        Debug.Log(x);
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+
+    float C(int ccc)
+    {
+        //float型の変数を宣言　変数名はnumber
+        float number;
+
+        //引数の変数を1.5倍したデータをnumberへ割り当て
+        number = ccc * 1.5f;
+
+        //返り値としてnumberのデータを返す
+        return number;
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/Example4.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Example4 : MonoBehaviour
+{
+    [SerializeField]
+    public int hp;
+}
