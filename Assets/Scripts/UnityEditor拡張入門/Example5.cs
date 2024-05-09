2ee547f6f2b96a8340b5a5fee4acda166fade22d
atenai <alfamod47@yahoo.co.jp>
2024-05-09 18:33:53 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/Example5.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Example5 : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)]
+    int hp;
+}
