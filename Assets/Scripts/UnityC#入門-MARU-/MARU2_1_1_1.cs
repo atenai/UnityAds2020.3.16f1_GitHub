45440795fcebef81bc99d2424ecc83a88edee63c
atenai <alfamod47@yahoo.co.jp>
2024-05-14 19:09:42 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_1_1_1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU2_1_1_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+
+    /// <summary>
+    /// テストです。
+    /// </summary>
+    /// <param name="aaa">引数の説明が書き込めます</param> 
+    void A(int aaa)
+    {
+
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/AnimatorParameterExample.cs
@@ -0,0 +1,28 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [RequireComponent(typeof(Animator))]
+// public class AnimatorParameterExample : MonoBehaviour
+// {
+//     //すべてのタイプのパラメーターを取得
+//     [AnimatorParameter]
+//     public string param;
+
+//     //Floatのみ
+//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+//     public string floatParam;
+
+//     //Intのみ
+//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+//     public string intParam;
+
+//     //Boolのみ
+//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+//     public string boolParam;
+
+//     //Triggerのみ
+
+//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+//     public string triggerParam;
+// }
