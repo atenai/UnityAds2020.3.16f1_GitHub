718e6b50814a9bd4959c92a484fea11e3fc82f68
atenai <alfamod47@yahoo.co.jp>
2024-05-15 18:56:37 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_2_1_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU2_2_1_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        // float a = 1.5f;
+        // string b = "テスト";
+
+        var a = 1.5f;
+        var b = "テスト";
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
--- Assets/Scripts/UnityEditor拡張入門/AnimatorParameterExample.cs
+++ Assets/Scripts/UnityEditor拡張入門/AnimatorParameterExample.cs
@@ -1,28 +1,54 @@
+// using System;
 // using System.Collections;
 // using System.Collections.Generic;
+// using TMPro;
+// using UnityEditor;
+// using UnityEditor.Animations;
 // using UnityEngine;
 
 // [RequireComponent(typeof(Animator))]
 // public class AnimatorParameterExample : MonoBehaviour
 // {
-//     //すべてのタイプのパラメーターを取得
-//     [AnimatorParameter]
-//     public string param;
+// 	//すべてのタイプのパラメーターを取得
+// 	[AnimatorParameter]
+// 	public string param;
 
-//     //Floatのみ
-//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
-//     public string floatParam;
+// 	//Floatのみ
+// 	[AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+// 	public string floatParam;
 
-//     //Intのみ
-//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
-//     public string intParam;
+// 	//Intのみ
+// 	[AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+// 	public string intParam;
 
-//     //Boolのみ
-//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
-//     public string boolParam;
+// 	//Boolのみ
+// 	[AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+// 	public string boolParam;
 
-//     //Triggerのみ
+// 	//Triggerのみ
 
-//     [AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
-//     public string triggerParam;
+// 	[AnimatorParameter(AnimatorParameterAttribute.ParameterType)]
+// 	public string triggerParam;
+
+// 	AnimatorController GetAnimatorController(SerializedProperty property)
+// 	{
+// 		var component = property.serializedObject.targetObject as Component;
+
+// 		if (component == null)
+// 		{
+// 			Debug.LogException(new InvalidCastException("Couldn't cast targetObject"));
+// 		}
+
+// 		var anim = component.GetComponent<Animator>();
+
+// 		if (anim == null)
+// 		{
+// 			var exception = new MissingComponentException("Missing Animator Component");
+
+// 			Debug.LogException(exception);
+// 			return null;
+// 		}
+
+// 		return anim.runtimeAnimatorController as AnimatorController;
+// 	}
 // }
