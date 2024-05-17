96b8aa375f50e0803e9337a624d9e603fc71fa29
atenai <alfamod47@yahoo.co.jp>
2024-05-17 18:20:37 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_3_1_1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU2_3_1_1 : MonoBehaviour
+{
+    // int a = default;
+    // string b = default;
+    // bool c = default;
+
+    int a;
+    string b;
+    bool c;
+
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        Debug.Log(a);
+        Debug.Log(b);
+        Debug.Log(c);
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/DisableExample.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DisableExample : MonoBehaviour
+{
+    //[Disable]
+    public string hoge = "hoge";
+
+    //[Disable]
+    public int fuga = 1;
+
+    //[Disable]
+    public AudioType audioType = AudioType.ACC;
+}
