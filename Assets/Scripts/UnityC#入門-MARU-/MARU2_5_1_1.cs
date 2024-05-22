9da5ecc1a528a2e0b1cdf5bb4b6f5f1927c2d08d
atenai <alfamod47@yahoo.co.jp>
2024-05-22 18:35:24 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_5_1_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU2_5_1_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        int score = 1000;
+
+        Debug.Log("あなたのスコアは" + score + "です。");
+        Debug.Log($"あなたのスコアは{score}です。");
+        Debug.Log(string.Format("あなたのスコアは{0}です。", score));
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/SceneNameExample.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneNameExample : MonoBehaviour
+{
+    //[SceneName]
+    public string sceneName;
+
+    //無効状態のシーンも表示する
+    //[SceneName(false)]
+    public string sceneName2;
+}
