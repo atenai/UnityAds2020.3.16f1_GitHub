96a25e8a8c4ba398a62eb39544761f0742d6a516
atenai <alfamod47@yahoo.co.jp>
2024-05-27 18:25:15 +0900

    エディタ拡張

--- Assets/Scripts/UnityEditor拡張入門/DoCreateScriptAsset.cs
+++ Assets/Scripts/UnityEditor拡張入門/DoCreateScriptAsset.cs
@@ -34,4 +34,18 @@ public class DoCreateScriptAsset : EndNameEditAction
 
         ProjectWindowUtil.ShowCreatedAsset(asset);
     }
+
+    [MenuItem("Assets/Create ExampleAssets")]
+    static void CreateExampleAssets()
+    {
+        var resourceFile = Path.Combine(EditorApplication.applicationContentsPath, "Resources/ScriptTemplates/81-C# Script-NewBehaviourScript.cs.txt");
+
+        //cs は "cs Script Icon"
+        //js は "js Script Icon"
+        Texture2D csIcon = EditorGUIUtility.IconContent("cs Script Icon").image as Texture2D;
+
+        var endNameEditAction = ScriptableObject.CreateInstance<DoCreateScriptAsset>();
+
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, endNameEditAction, "NewBehaviourScript.cs", csIcon, resourceFile);
+    }
 }
