3777a9c114359d2a3c3361f30cdb7b23778d76c9
atenai <alfamod47@yahoo.co.jp>
2024-05-29 19:46:49 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_8_1_2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_8_1_2 : MonoBehaviour
+{
+
+    //配列
+    int[] scoreArray = { 10, 20, 30, 40, 50 };
+
+    //List
+    List<int> scoreList = new List<int>() { 10, 20, 30, 40, 50 };
+
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        //List<int> scoreList;
+        //scoreList = new List<int>();
+
+        // scoreList.Add(10);
+        // scoreList.Add(20);
+        // scoreList.Add(30);
+        // scoreList.Add(40);
+        // scoreList.Add(50);
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/DoCreateMaterialAsset.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.ProjectWindowCallback;
+using UnityEditor;
+
+public class DoCreateMaterialAsset : EndNameEditAction
+{
+    public override void Action(int instanceId, string pathName, string resourceFile)
+    {
+        var mat = (Material)EditorUtility.InstanceIDToObject(instanceId);
+
+        //強制的にマテリアルを赤色にする
+        mat.color = Color.red;
+
+        AssetDatabase.CreateAsset(mat, pathName);
+        AssetDatabase.ImportAsset(pathName);
+        ProjectWindowUtil.ShowCreatedAsset(mat);
+    }
+}
