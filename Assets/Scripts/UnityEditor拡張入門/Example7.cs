f43e8ccbaf56bcf1698127b0adb8fc90d53b47dd
atenai <alfamod47@yahoo.co.jp>
2024-06-02 13:26:59 +0900

    MARU

---
+++ Assets/Scripts/UnityEditor拡張入門/Example7.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Example7
+{
+    [MenuItem("Example7/Create Cube")]//上のメニューに項目の追加をする
+    static void CreateCube()//必ずstatic型
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);//キューブを生成
+    }
+}
