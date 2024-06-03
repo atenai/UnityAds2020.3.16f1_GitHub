a24ac73049ae16844f70cceae7fa57a88895a3a3
atenai <alfamod47@yahoo.co.jp>
2024-06-03 23:33:07 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_8_1_5.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_8_1_5 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        //List 初期化
+        List<int> scoreList = new List<int>() { 10, 20, 30, 40, 50 };
+
+        //挿入
+        scoreList.RemoveAt(2);
+
+        //List 全要素の確認
+        for (int i = 0; i < scoreList.Count; i++)
+        {
+            UnityEngine.Debug.Log("Listの要素 = " + scoreList[i]);
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/Example8.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Example8
+{
+    [MenuItem("Example8/Create Cube")]//上のメニューに項目の追加をする
+    static void CreateCube()//必ずstatic型
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);//キューブを生成
+        Undo.RegisterCreatedObjectUndo(cube, "Create Cube");//キューブを削除する（元に戻す）
+    }
+}
