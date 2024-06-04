3adc262f283b6774b18584cd08381a326354cd37
atenai <alfamod47@yahoo.co.jp>
2024-06-04 23:50:28 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_9_1_1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_9_1_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        //配列の初期化
+        int[] scoreArray = { 10, 20, 30, 40, 50 };
+
+        foreach (int a in scoreArray)
+        {
+            UnityEngine.Debug.Log(a);
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
