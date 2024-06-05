1832f087fc1448271e728b985dc188c8129045e2
atenai <alfamod47@yahoo.co.jp>
2024-06-05 20:48:30 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/Example9.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Example9
+{
+    [MenuItem("Example9/Random Rotate")]//上のメニューに項目の追加をする
+    static void RandomRotate()//必ずstatic型
+    {
+        var transform = Selection.activeTransform;
+
+        if (transform)
+        {
+            transform.rotation = Random.rotation;
+        }
+    }
+}
