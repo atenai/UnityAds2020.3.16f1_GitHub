0cae09ea4d84f306bc9d758029599756e6155c7b
atenai <alfamod47@yahoo.co.jp>
2024-06-08 16:30:24 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_11_1_1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_11_1_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        int a = 10;
+
+        //if文基本構文
+        if (a == 100)
+        {
+            UnityEngine.Debug.Log("テスト");
+        }
+
+        //処理内容が一行のみの時ブロック{}を省略できる
+        if (a == 100)
+            UnityEngine.Debug.Log("テスト");
+
+        //処理内容が一行のみの時ブロック{}を省略できる 真横に書ける
+        if (a == 100) UnityEngine.Debug.Log("テスト");
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/Example11.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Example11
+{
+    [MenuItem("Example11/Random Rotate")]//上のメニューに項目の追加をする
+    static void RandomRotate()//必ずstatic型
+    {
+        var transform = Selection.activeTransform;
+
+        if (transform)
+        {
+            Undo.willFlushUndoRecord += () => Debug.Log("flush");
+
+            Undo.postprocessModifications += (modifications) =>
+            {
+                Debug.Log("modifications");
+                return modifications;
+            };
+
+            Undo.RecordObject(transform, "Rotate " + transform.name);//Undo.RecordObject関数を実行すると「変更前」のTransformの値をUndoスタックに保存できる
+            Debug.Log("recorded");
+
+            transform.rotation = Random.rotation;
+            Debug.Log("changed");
+        }
+    }
+}
