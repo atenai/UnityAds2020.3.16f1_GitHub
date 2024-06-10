c02c8883c365b484d401bd794ec452ad39a92723
atenai <alfamod47@yahoo.co.jp>
2024-06-10 23:43:07 +0900

    メッシュの拡頂点にスフィアを生成するように作成

--- Assets/Scripts/Mesh/GetVtx.cs
+++ Assets/Scripts/Mesh/GetVtx.cs
@@ -8,12 +8,21 @@ using UnityEngine;
 /// </summary>
 public class GetVtx : MonoBehaviour
 {
+    [SerializeField] GameObject prefab;
+
     void Start()
     {
+        //Mesh Filter コンポーネントはメッシュへの参照を保存します。
+        //Mesh Filter コンポーネントは同じゲームオブジェクト上の Mesh Renderer コンポーネントと連動し、Mesh Renderer は Mesh Filter が参照するメッシュを描画します。
+        //https://docs.unity3d.com/ja/2022.2/Manual/class-MeshFilter.html
         Mesh myMesh = GetComponent<MeshFilter>().mesh;
         for (int i = 0; i < myMesh.vertices.Length; i++)
         {
             Debug.Log(myMesh.vertices[i]);
+            // プレファブをインスタンス化し、親オブジェクトの子として設定
+            //GameObject gameObject = Instantiate(prefab, myMesh.vertices[i] + this.transform.position, Quaternion.identity);
+            GameObject gameObject = Instantiate(prefab, myMesh.vertices[i], Quaternion.identity);
+            gameObject.transform.SetParent(this.transform, false);//第二引数をfalseにするとローカル座標を破棄した子オブジェクトになる（その為、通常はtrueにするが今回の場合の例外）
         }
     }
 }
