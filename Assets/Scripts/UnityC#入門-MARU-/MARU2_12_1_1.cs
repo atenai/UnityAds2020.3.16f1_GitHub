0ccdf92495e01a8cba5988b1d27d46e973025fb5
atenai <alfamod47@yahoo.co.jp>
2024-06-10 19:32:55 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_12_1_1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_12_1_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+
+#if UNITY_ANDROID//Androidの場合の処理
+
+//Androidの処理を書く
+
+#endif//Androidの処理範囲終了
+
+#if UNITY_IOS//iOSの場合の処理
+
+//iOSの処理を書く
+
+#endif//iOSの処理範囲終了
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
