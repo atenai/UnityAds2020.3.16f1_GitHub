8d0db7b318fb0d7644ce2d2f83dc02b4f45e9bc3
atenai <alfamod47@yahoo.co.jp>
2024-06-12 16:12:41 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        PlayerInfo playerInfo;
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/PlayerInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int hp;
+}
