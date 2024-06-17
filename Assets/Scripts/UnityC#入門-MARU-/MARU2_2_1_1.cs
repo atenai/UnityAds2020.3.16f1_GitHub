f8e7bfd76a08d75f0d64e7dcb95cb43ec4bf65a0
atenai <alfamod47@yahoo.co.jp>
2024-06-17 19:28:09 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_2_1_1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_2_1_1 : MonoBehaviour
+{
+    //GameObject型の変数TestObjectを宣言
+    public GameObject TestObject;
+
+    void Start()
+    {
+        //Debug.Log("Start");
+
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/NewBehaviourScript7.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+
+public class NewBehaviourScript7
+{
+    //必ず宝くじが当選するメソッド
+    [MenuItem("UndoExample/Undo/RevertAllInCurrentGroup")]//上のメニューに項目の追加をする
+    static void RevertAllInCurrentGroup()
+    {
+        GameObject ticket = new GameObject("Ticket");
+
+        Undo.RegisterCreatedObjectUndo(ticket, "宝くじ買いました");
+
+        //チケットの番号はこれです
+        int number = ticket.GetInstanceID();
+
+        //当選番号はこれです
+        int winningNumber = 1234;
+
+        if (ticket.GetInstanceID() != winningNumber)
+        {
+            //当たりませんでした
+            //宝くじを買ったのをなかったことにします
+
+            Undo.RevertAllInCurrentGroup();
+        }
+    }
+}
