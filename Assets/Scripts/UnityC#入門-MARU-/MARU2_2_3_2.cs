648f86955e13dbc3d7e01612c2fcc84c7e47e624
atenai <alfamod47@yahoo.co.jp>
2024-06-20 15:23:46 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_2_3_2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MARU2_2_3_2 : MonoBehaviour
+{
+    //GameObject型の変数TestObjectを宣言
+    public GameObject TestObject;
+
+    void Start()
+    {
+        //Debug.Log("Start");
+
+        TestObject.transform.Translate(1.0f, 10.0f, 100.0f);
+        TestObject.transform.Translate(1.0f, 10.0f, 100.0f);
+        TestObject.transform.Translate(1.0f, 10.0f, 100.0f);
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
