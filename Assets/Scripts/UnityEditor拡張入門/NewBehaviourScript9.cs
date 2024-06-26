b786d76aa86943236f9a20502238189756ea2296
atenai <alfamod47@yahoo.co.jp>
2024-06-26 19:38:45 +0900

    MARU

---
+++ Assets/Scripts/UnityEditor拡張入門/NewBehaviourScript9.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+using UnityEditor.Callbacks;
+using System.IO;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.SceneManagement;
+
+public class NewBehaviourScript9
+{
+    // [PostProcessScene]
+    // static void OnPostProcessScene()
+    // {
+    //     //現在のマルチシーン情報を取得
+    //     foreach (var sceneSetup in EditorSceneManager.GetSceneManagerSetup())
+    //     {
+    //         var scene = EditorSceneManager.GetSceneByPath(sceneSetup.path);
+    //         var go = new GameObject("OnPostProcessScene: " + scene.name);
+
+    //         EditorSceneManager.MoveGameObjectToScene(go, scene);
+    //     }
+    // }
+}
