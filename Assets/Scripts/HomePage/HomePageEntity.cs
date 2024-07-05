1296384de71c7c89a7ac8a8a5060a8b5e04a91ea
atenai <alfamod47@yahoo.co.jp>
2024-07-06 02:48:21 +0900

    URLを追加

--- Assets/Scripts/HomePage/HomePageEntity.cs
+++ Assets/Scripts/HomePage/HomePageEntity.cs
@@ -3,9 +3,11 @@ public class HomePageEntity
     public readonly string URL_UnityEditor_exe = "http://kagring.blog.fc2.com/blog-entry-13.html";
     public readonly string URL_DeepL = "https://www.deepl.com/ja/translator";
     public readonly string URL_Window = "https://www.fast-system.jp/unity-fullscreen-windowed-fixed-resizable/";
-    public readonly string URL_UnityDocumentation = "https://docs.unity3d.com/ja/2023.2/ScriptReference/index.html";
+    public readonly string URL_UnityDocumentation = "https://docs.unity3d.com/ja/2018.4/Manual/PlatformDependentCompilation.html";
     public readonly string URL_UnityTimeDeltaTime = "https://xr-hub.com/archives/14465";
     public readonly string URL_BingAI = "https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx";
     public readonly string URL_ChatGPT = "https://chatgpt.com/";
     public readonly string URL_ProductName = "https://fall-and-fall.hatenablog.com/entry/unity/build/change-exe-name";
+    public readonly string URL_DebugLogColor = "https://qiita.com/phi/items/d98a177f4e12426d9f4f";
+    public readonly string URL_Find = "https://www.sejuku.net/blog/53526";
 }
