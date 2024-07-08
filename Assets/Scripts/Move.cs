c8689e98035ee272a9fef00506d5b72ea9de57cd
atenai <alfamod47@yahoo.co.jp>
2024-07-08 19:55:37 +0900

    フォルダーの整理

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //Œ»İ‚ÌˆÊ’u‚ğæ“¾
         Vector3 pos = this.gameObject.transform.position;
-        //Œ»İ‚ÌˆÊ’u‚©‚çx•ûŒü‚É1ˆÚ“®‚·‚é
         this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z + 0.1f);
     }
 }
--- Assets/Scripts/ゲームオブジェクトの切り替え/ObjectChange.cs
+++ Assets/Scripts/ゲームオブジェクトの切り替え/ObjectChange.cs
@@ -12,23 +12,18 @@ public class ObjectChange : MonoBehaviour
     void Start()
     {
         count = 0;
-        //cubeArray[0番目]のゲームオブジェクトをインスタンスして
-        //as演算子でGameObjectの配列型からGameObject型に変更してcubeObjに格納
+
         cubeObj = GameObject.Instantiate(cubeArray[count]) as GameObject;
     }
 
     public void CubeSet()
     {
-        //CubeSet()関数が呼ばれたら中身を実行する
-        //cubeObjを破棄
         Destroy(cubeObj);
-        //番数目を+1する
+
         count++;
-        //cubeArray[0番目]のゲームオブジェクトをインスタンスして
-        //as演算子でGameObjectの配列型からGameObject型に変更してcubeObjに格納
+
         cubeObj = GameObject.Instantiate(cubeArray[count]) as GameObject;
-        //最大数(2)になったらカウントを-1に変更する
-        //そうすれば次の+1で0番目に戻る事ができる為
+
         if (count == 2)
         {
             count = -1;
