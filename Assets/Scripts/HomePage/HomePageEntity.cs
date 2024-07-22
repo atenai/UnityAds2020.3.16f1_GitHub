1732828e63f01ae6115c83827e87ab7422d32d49
atenai <alfamod47@yahoo.co.jp>
2024-07-22 21:52:10 +0900

    URLを追加

--- Assets/Scripts/HomePage/HomePageEntity.cs
+++ Assets/Scripts/HomePage/HomePageEntity.cs
@@ -10,4 +10,21 @@ public class HomePageEntity
     public readonly string URL_ProductName = "https://fall-and-fall.hatenablog.com/entry/unity/build/change-exe-name";
     public readonly string URL_DebugLogColor = "https://qiita.com/phi/items/d98a177f4e12426d9f4f";
     public readonly string URL_Find = "https://www.sejuku.net/blog/53526";
+    public readonly string URL_Framerate = "https://cgbox.jp/2023/09/26/unity-framerate/";
+    public readonly string URL_ScriptableObject1 = "https://qiita.com/4_mio_11/items/a7d8967b853cef4385cd";
+    public readonly string URL_ScriptableObject2 = "https://note.com/citronworld/n/n47965ddec2ec";
+    public readonly string URL_Mesh = "https://qiita.com/Torihaka1/items/910d1365dddc031aecaf";
+    public readonly string URL_MeshFilter = "https://docs.unity3d.com/ja/2022.2/Manual/class-MeshFilter.html";
+    public readonly string URL_Stack = "https://gametukurikata.com/csharp/stack";
+    public readonly string URL_FindReferenceInScene = "https://atelier-hinata.hatenablog.com/entry/2020/07/30/111702#Find-Reference-In-Scene%E3%81%A7%E7%8A%AF%E4%BA%BA%E6%8E%A2%E3%81%97";
+    public readonly string URL_Addressable1 = "https://qiita.com/k7a/items/b4fd298bcb64dc968ad1";
+    public readonly string URL_Addressable2 = "https://original-game.com/unity-how-to-use-addressables/";
+    public readonly string URL_Addressable3 = "https://note.com/hikohiro/n/n6cef6cc792a3";
+    public readonly string URL_ExecutionOrder = "https://docs.unity3d.com/ja/2018.4/Manual/ExecutionOrder.html";
+    public readonly string URL_Attribute = "https://tsubakit1.hateblo.jp/entry/2015/01/03/203843";
+    public readonly string URL_PrefabVariant = "https://qiita.com/miikun109/items/cb8a0c01f7f2a460737d";
+    public readonly string URL_ShortcutKey = "https://santerabyte.com/unity-play-pause-step-shortcut/";
+    public readonly string URL_ScriptExecutionOrder = "https://ekulabo.com/execution-order";
+    public readonly string URL_GomaFrontier = "https://gomafrontier.com/";
+
 }
