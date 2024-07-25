63de6f7cdbc35d881f88ab54c4179eb8b99f9dd1
atenai <alfamod47@yahoo.co.jp>
2024-07-25 16:56:59 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_3_1_1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq.Expressions;
+
+public class MARU2_3_1_1 : MonoBehaviour
+{
+    //BoxCollider型の変数Aを宣言
+    public BoxCollider A;
+
+    void Start()
+    {
+        //Debug.Log("Start");
+
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+}
