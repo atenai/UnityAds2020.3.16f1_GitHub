26f7b0aeba9ebd1cae491752cc3720949e9e362f
atenai <alfamod47@yahoo.co.jp>
2024-07-26 13:27:01 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_3_2_1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MARU2_3_2_1 : MonoBehaviour
+{
+    //string型の変数Bを宣言
+    public string B;
+
+    void Start()
+    {
+        //Debug.Log("Start");
+
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+
+    //変数Bをコンソールに表示する関数
+    public void CCC()
+    {
+        Debug.Log(B);
+    }
+}
