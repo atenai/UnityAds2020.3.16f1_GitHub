d91153f73be9b51349a12eec14efd5c23486aca4
atenai <alfamod47@yahoo.co.jp>
2024-08-06 19:26:31 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/Example17.cs
@@ -0,0 +1,14 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// ExampleInspecto2.csと紐づいている
+/// </summary> 
+public class Example17 : MonoBehaviour
+{
+
+}
+#endif
---
+++ Assets/Scripts/UnityEditor拡張入門/ExampleInspector2.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Example17.csと紐づいているクラス
+/// </summary> 
+[CustomEditor(typeof(Example17))]
+public class ExampleInspecto2 : Editor
+{
+    void OnSceneGUI()
+    {
+        Tools.current = Tool.None;
+        var component = target as Example17;
+
+        var transform = component.transform;
+        transform.position = Handles.PositionHandle(transform.position, transform.rotation);
+    }
+}
+#endif
