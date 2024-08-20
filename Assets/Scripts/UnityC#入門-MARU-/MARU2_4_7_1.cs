f41a8796356b3ffd01f83a2ee35b8042f3f540a1
atenai <alfamod47@yahoo.co.jp>
2024-08-20 19:17:27 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_4_7_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;//Text型が含まれている名前空間を追加
+
+public class MARU2_4_7_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start");
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+
+    public void A()
+    {
+        Debug.Log("ボタンが押されました");
+    }
+}
