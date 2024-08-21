ab7904b815e0f051a75ea7b3392e3ee63f2764f8
atenai <alfamod47@yahoo.co.jp>
2024-08-21 18:10:28 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/Example29.cs
@@ -0,0 +1,14 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// ExampleInspecto14.csと紐づいている
+/// </summary> 
+public class Example29 : MonoBehaviour
+{
+
+}
+#endif
---
+++ Assets/Scripts/UnityEditor拡張入門/ExampleInspector14.cs
@@ -0,0 +1,51 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditorInternal;
+
+/// <summary>
+/// Example29.csと紐づいているクラス
+/// </summary> 
+[CustomEditor(typeof(Example29))]
+public class ExampleInspecto14 : Editor
+{
+    Example29 component;
+
+    int windowID = 1234;
+    Rect windowRect;
+
+    void OnEnable()
+    {
+        //ここは様式美
+        Tools.current = Tool.None;
+        component = target as Example29;
+        //ここは様式美
+    }
+
+    void OnSceneGUI()
+    {
+        Handles.BeginGUI();
+
+        windowRect = GUILayout.Window
+        (
+            windowID,
+            windowRect,
+            (id) =>
+            {
+                EditorGUILayout.LabelField("Label");
+                EditorGUILayout.ToggleLeft("Toggle", false);
+                GUILayout.Button("Button");
+                GUI.DragWindow();
+
+            },
+            "Window",
+            GUILayout.Width(100)
+        );
+
+        Handles.EndGUI();
+    }
+}
+#endif
