61221505695dfda8a301a16919541131d60c70c1
atenai <alfamod47@yahoo.co.jp>
2024-08-31 19:39:50 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/CustomEditorGUI.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomEditorGUI
+{
+    private static GUIStyle spriteStyle = new GUIStyle();
+
+    public static Sprite SpriteField(Rect rect, Sprite sprite)
+    {
+        var evt = Event.current;
+        int id = GUIUtility.GetControlID(FocusType.Keyboard, rect);
+
+        if (evt.type == EventType.Repaint)
+        {
+            //サムネ形式の背景を描画
+            EditorStyles.objectFieldThumb.Draw(rect, GUIContent.none, id, false);
+
+            if (sprite)
+            {
+                //スプライトからプレビュー用のテクスチャを取得
+                var spriteTexture = AssetPreview.GetAssetPreview(sprite);
+                if (spriteTexture)
+                {
+                    spriteStyle.normal.background = spriteTexture;
+                    //スプライトを描画
+                    spriteStyle.Draw(rect, false, false, false, false);
+                }
+            }
+        }
+
+        if (rect.Contains(evt.mousePosition))
+        {
+            var on = DragAndDrop.activeControlID == id;
+            EditorStyles.objectFieldThumb.Draw(rect, GUIContent.none, id, on);
+
+            switch (evt.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+
+                    if (DragAndDrop.objectReferences.Length == 1)
+                    {
+                        DragAndDrop.AcceptDrag();
+                    }
+
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+                    break;
+
+                case EventType.DragExited:
+
+                    if (DragAndDrop.objectReferences.Length == 1)
+                    {
+                        var reference = DragAndDrop.objectReferences[0] as Sprite;
+                        if (reference != null)
+                        {
+                            sprite = reference;
+                            HandleUtility.Repaint();
+                        }
+                    }
+                    break;
+            }
+        }
+
+        var buttonRect = new Rect(rect);
+        //加工
+        buttonRect.x += buttonRect.width * 0.5f;
+        buttonRect.width *= 0.5f;
+        buttonRect.y += rect.height - 16;
+        buttonRect.height = 16;
+
+        //キーを押した時
+        //エンターキーである時
+        //そして操作しているのがこのGUIであるとき
+        var hitEnter = evt.type == EventType.KeyDown && (evt.keyCode == KeyCode.Return || evt.keyCode == KeyCode.KeypadEnter) && EditorGUIUtility.keyboardControl == id;
+        //ボタンを押した時、またはエンターキーを押した時にオブジェクトピッカーを表示
+        if (GUI.Button(buttonRect, "select", EditorStyles.objectFieldThumb.name + "Overlay2") || hitEnter)
+        {
+            //どのGUIで表示したか判断できるようにコントロールIDを渡す
+            EditorGUIUtility.ShowObjectPicker<Sprite>(sprite, false, "", id);
+            evt.Use();
+            GUIUtility.ExitGUI();
+        }
+
+        //オブジェクトピッカーがこのGUIのためのものであるか判断
+        if (evt.commandName == "ObjectSelectorUpdated" && id == EditorGUIUtility.GetObjectPickerControlID())
+        {
+            sprite = EditorGUIUtility.GetObjectPickerObject() as Sprite;
+
+            //描画するスプライトが変更されたので再描画
+            HandleUtility.Repaint();
+        }
+
+        return sprite;
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/NewBehaviourScript24.cs
@@ -0,0 +1,33 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// エディタウィンドウを作る為の金型
+/// </summary>
+public class NewBehaviourScript24 : EditorWindow
+{
+
+    [MenuItem("Kashiwabara/NewBehaviourScript24")]
+    static void Open()
+    {
+        GetWindow<NewBehaviourScript24>();
+    }
+
+    public static Sprite SpriteField(Sprite sprite, params GUILayoutOption[] options)
+    {
+        // スプライトフィールドのラベルを表示
+        EditorGUILayout.LabelField(", ", options);
+
+        // ラベルの位置を取得
+        var rect = GUILayoutUtility.GetLastRect();
+
+        // スプライトフィールドを描画し、選択されたスプライトを返す
+        return (Sprite)EditorGUI.ObjectField(rect, sprite, typeof(Sprite), allowSceneObjects: false);
+    }
+}
+#endif
