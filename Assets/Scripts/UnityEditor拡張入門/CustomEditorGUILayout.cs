d65ee29c79a47d39e9d3ad600626ddb834f79fd7
atenai <alfamod47@yahoo.co.jp>
2024-09-02 17:14:42 +0900

    エディタ拡張

---
+++ Assets/Scripts/UnityEditor拡張入門/CustomEditorGUILayout.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomEditorGUILayout
+{
+    public static Sprite SpriteField(Sprite sprite, params GUILayoutOption[] options)
+    {
+        EditorGUILayout.LabelField("", "", options);
+        var rect = GUILayoutUtility.GetLastRect();
+        return CustomEditorGUI.SpriteField(rect, sprite);
+    }
+}
