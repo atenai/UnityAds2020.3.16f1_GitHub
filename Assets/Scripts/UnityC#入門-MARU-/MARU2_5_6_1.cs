1d0133c1c2a0920897c393d8c338beb2d9011d4a
atenai <alfamod47@yahoo.co.jp>
2024-09-03 17:36:51 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_5_6_1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;//Text型が含まれている名前空間を追加
+
+public class MARU2_5_6_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start関数呼び出し");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    void LateUpdate()
+    {
+        Debug.Log("LateUpdate");
+    }
+}
---
+++ Assets/Scripts/UnityEditor拡張入門/NewBehaviourScript25.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// エディタウィンドウを作る為の金型
+/// </summary>
+public class NewBehaviourScript25 : EditorWindow
+{
+
+    [MenuItem("Kashiwabara/SpriteEditor")]
+    static void Open()
+    {
+        GetWindow<NewBehaviourScript25>();
+    }
+
+    private Sprite sprite1, sprite2;
+
+    void OnGUI()
+    {
+        sprite1 = CustomEditorGUI.SpriteField(new Rect(134, 1, 128, 128), sprite1);
+        sprite2 = CustomEditorGUILayout.SpriteField(sprite2, GUILayout.Width(128), GUILayout.Height(128));
+    }
+}
+#endif
