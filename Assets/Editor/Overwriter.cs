6c84b347fea276714f2769bfd8b0f72010f6c49c
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-09-13 04:27:05 +0900

    MARU

--- Assets/Editor/Overwriter.cs
+++ Assets/Editor/Overwriter.cs
@@ -6,57 +6,37 @@ using UnityEditor;
 
 public class Overwriter : AssetPostprocessor
 {
+    const string message = "\"{0}.{1}\"という名前のアセットがすでにこの場所にあります。アセットを置き換えますか？";
+
     //hoge.pngがある状態でhoge.pngをインポート
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath)
     {
-        if (Event.current.type != EventType.DragPerform)
-        {
-            return;
-        }
-
-        //リネーム後のhoge1.pngでインポートされていることを確認
-        var hoge1 = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/hoge1.png");
-        Debug.Log(hoge1);
-
-        //hoge.pngのパスが格納されていることを確認
-        foreach (var path in DragAndDrop.paths)
-        {
-            Debug.Log(path);
-        }
-
         //スクリプトの編集によるインポート時はEvent.currentはnullなので、nullチェックをする
         if (Event.current == null || Event.current.type != EventType.DragPerform)
         {
             return;
         }
 
-        // var result = EditorUtility.DisplayDialogComplex(asset.originalAssetPath, overwriteMessage, "置き換える", "両方とも残す", "中止");
-
-        // if (result == 0)
-        // {
-        //     asset.Overwrite();
-        // }
-        // else if (result == 2)
-        // {
-        //     asset.Delete();
-        // }
-
         foreach (var assetPath in importedAssets)
         {
             //インポートされたアセットを監視
+            var asset = new OverwriteAsset(assetPath);
+
+            if (asset.exists)
+            {
+                var overwriteMessage = string.Format(message, asset.filename, asset.extension);
+                var result = EditorUtility.DisplayDialogComplex(asset.originalAssetPath, overwriteMessage, "置き換える", "両方とも残す", "中止");
+
+                if (result == 0)
+                {
+                    asset.Overwrite();
+                }
+                else if (result == 2)
+                {
+                    asset.Delete();
+                }
+            }
         }
     }
-
-    // public void Overwrite()
-    // {
-    //     FileUtil.ReplaceFile(assetPath, originalAssetPath);
-    //     Delete();
-    //     AssetDatabase.ImportAsset(originalAssetPath);
-    // }
-
-    // public void Delete()
-    // {
-    //     AssetDatabase.DeleteAsset(assetPath);
-    // }
 }
 #endif
