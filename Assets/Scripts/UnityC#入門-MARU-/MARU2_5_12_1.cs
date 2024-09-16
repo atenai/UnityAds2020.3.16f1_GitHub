fbddcb33520075c280e646a1c7e09a72a4f1896f
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-09-16 20:08:20 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU2_5_12_1.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;//Text型が含まれている名前空間を追加
+
+public class MARU2_5_12_1 : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Start関数呼び出し");
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update");
+    }
+
+    void LateUpdate()
+    {
+        //Debug.Log("LateUpdate");
+    }
+
+    void FixedUpdate()
+    {
+        //Debug.Log("FixedUpdate");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name + "との接触開始");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name + "との接触中");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name + "との接触終了");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.name + "との接触開始");
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.name + "との接触中");
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.name + "との接触終了");
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log(name + "のコライダー範囲にマウスが入りました");
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log(name + "のコライダー範囲にマウスが入り続けています");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log(name + "のコライダー範囲にマウスが出ました");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(name + "のコライダー範囲にマウス左が押されました");
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log(name + "のコライダー範囲にマウス左ドラッグを開始しました" + "(コライダーの範囲外でも有効)");
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log(name + "のコライダー範囲内でマウス左が離されました");
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log(name + "からマウス左が離されました" + "(コライダーの範囲外でも有効)");
+    }
+}
