485140f0124ce831f2461938cc599725aecc4e04
atenai <alfamod47@yahoo.co.jp>
2024-09-25 17:20:01 +0900

    エディタ拡張

--- Assets/Scripts/UnityEditor拡張入門/SpritePreview.cs
+++ Assets/Scripts/UnityEditor拡張入門/SpritePreview.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CustomPreview(typeof(AnimationClip))]
 public class SpritePreview : ObjectPreview
@@ -21,6 +23,22 @@ public class SpritePreview : ObjectPreview
     public override void OnPreviewGUI(Rect r, GUIStyle background)
     {
         GUI.Box(r, "表示領域");
+    }
+
+    private Sprite[] GetSprites(AnimationClip animationClip)
+    {
+        var sprites = new Sprite[0];
+
+        if (animationClip != null)
+        {
+            var editorCurveBinding = EditorCurveBinding.PPtrCurve("", typeof(SpriteRenderer), "m_Sprite");
+
+            var objectReferenceKeyframes = AnimationUtility.GetObjectReferenceCurve(animationClip, editorCurveBinding);
+
+            sprites = objectReferenceKeyframes.Select(objectReferenceKeyframe => objectReferenceKeyframe.value).OfType<Sprite>().ToArray();
+        }
+
+        return sprites;
     }
 }
 
