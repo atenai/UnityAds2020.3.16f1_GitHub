3ddc7ec76935e349174ee2a159646cb59caf5bc6
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-10-22 22:29:17 +0900

    MARU

--- Assets/Editor/TimeControl.cs
+++ Assets/Editor/TimeControl.cs
@@ -5,39 +5,49 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-public class TimeControl : MonoBehaviour
+public class TimeControl
 {
+    private double m_lastFrameEditorTime = 0;
     public bool isPlaying { get; private set; }
-    private float currentTime { get; set; }
-    private double lastFrameEditorTime { get; set; }
-    public float speed { get; set; }
+    public float minTime { get; private set; }
+    public float maxTime { get; private set; }
+    private float m_currentTime;
+    public float CurrentTime
+    {
+        get
+        {
+            m_currentTime = Mathf.Repeat(m_currentTime, maxTime);
+            m_currentTime = Mathf.Clamp(m_currentTime, minTime, maxTime);
+            return m_currentTime;
+        }
+        set
+        {
+            m_currentTime = value;
+        }
+    }
+    public float Speed { get; set; }
 
     public TimeControl()
     {
-        speed = 1;
-        EditorApplication.update += Update;
+        Speed = 1;
+        EditorApplication.update += TimeUpdate;
     }
 
-    public void Update()
+    public void TimeUpdate()
     {
         if (isPlaying)
         {
             var timeSinceStartup = EditorApplication.timeSinceStartup;
-            var deltaTime = timeSinceStartup - lastFrameEditorTime;
-            lastFrameEditorTime = timeSinceStartup;
-            currentTime += (float)deltaTime * speed;
+            var deltaTime = timeSinceStartup - m_lastFrameEditorTime;
+            m_lastFrameEditorTime = timeSinceStartup;
+            m_currentTime += (float)deltaTime * Speed;
         }
-    }
-
-    public float GetCurrentTime(float stopTime)
-    {
-        return Mathf.Repeat(currentTime, stopTime);
     }
 
     public void Play()
     {
         isPlaying = true;
-        lastFrameEditorTime = EditorApplication.timeSinceStartup;
+        m_lastFrameEditorTime = EditorApplication.timeSinceStartup;
     }
 
     public void Pause()
@@ -45,64 +55,15 @@ public class TimeControl : MonoBehaviour
         isPlaying = false;
     }
 
-    TimeControl timeControl = new TimeControl();
-    private void DrawPlayButton()
+    public void Stop()
     {
-        var playButtonContent = EditorGUIUtility.IconContent("PlayButton");
-        var pauseButtonContent = EditorGUIUtility.IconContent("PauseButton");
-        var previewButtonSettingsStyle = new GUIStyle("preButton");
-        var buttonContent = timeControl.isPlaying ? pauseButtonContent : playButtonContent;
-
-        EditorGUI.BeginChangeCheck();
-        var isPlaying = GUILayout.Toggle(timeControl.isPlaying, buttonContent, previewButtonSettingsStyle);
-        if (EditorGUI.EndChangeCheck())
-        {
-            if (isPlaying)
-            {
-                timeControl.Play();
-            }
-            else
-            {
-                timeControl.Pause();
-            }
-        }
+        isPlaying = false;
+        m_currentTime = 0;
     }
 
-    private void DrawSpeedSlider()
+    public void SetMinMaxTime(float minTime, float maxTime)
     {
-        var preSlider = new GUIStyle("preSlider");
-        var preSliderThumb = new GUIStyle("preSliderThumb");
-        var preLabel = new GUIStyle("preLabel");
-        var speedScale = EditorGUIUtility.IconContent("SpeedScale");
-
-        GUILayout.Box(speedScale, preLabel);
-        timeControl.speed = GUILayout.HorizontalSlider(timeControl.speed, 0, 10, preSlider, preSliderThumb);
-        GUILayout.Label(timeControl.speed.ToString("0.00"), preLabel, GUILayout.Width(40));
+        this.minTime = minTime;
+        this.maxTime = maxTime;
     }
-
-    // private List<Editor> GetSpriteEditors(params Sprite[] sprites)
-    // {
-    //     var type = Types.GetType("UnityEditor.SpriteInspector", "UnityEditor.dll");
-    //     var editors = new List<Editor>();
-
-    //     foreach (var sprite in sprites)
-    //     {
-    //         Editor _editor = Editor.CreateEditor(sprite, type);
-
-    //         if (_editor != null)
-    //         {
-    //             editors.Add(_editor);
-    //         }
-    //     }
-
-    //     return editors;
-    // }
-
-    // public void OnDisable()
-    // {
-    //     foreach (var spriteEditor in spriteEditors)
-    //     {
-    //         Object.DestroyImmediate(spriteEditor);
-    //     }
-    // }
 }
