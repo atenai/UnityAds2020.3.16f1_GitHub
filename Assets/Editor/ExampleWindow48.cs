072e76a1a481bf294a8e4aa855f11a3955073243
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-10-28 19:29:24 +0900

    エディタ拡張

--- Assets/Editor/ExampleWindow48.cs
+++ Assets/Editor/ExampleWindow48.cs
@@ -11,13 +11,35 @@ using UnityEditor.SceneManagement;
 using System.Linq;
 using UnityEditor.SearchService;
 
+/// <summary>
+/// テキストを作成するクラス
+/// </summary>
 public class ExampleWindow48 : EditorWindow
 {
-    [MenuItem("Kashiwabara/Example48")]
-    static void CreateTextAsset()
+    [MenuItem("Kashiwabara/Example48_1")]
+    static void CreateTextAsset1()
     {
-        var text = "text";
-        File.WriteAllText("Assets/New TextAsset.txt", text);
+        //生成する位置（パス）と名前
+        var path = "Assets/New TextAsset1.txt";
+        //生成したテキストの内容
+        var text = "text1";
+        File.WriteAllText(path, text);
+
+        //リフレッシュすることにより全体のアセットをインポートする
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Kashiwabara/Example48_2")]
+    static void CreateTextAsset2()
+    {
+        //生成する位置（パス）と名前
+        var path = "Assets/New TextAsset2.txt";
+        //生成したテキストの内容
+        var text = "text2";
+        File.WriteAllText(path, text);
+
+        //パスを指定して特定のアセットをインポートする
+        AssetDatabase.ImportAsset(path);
     }
 }
 #endif
