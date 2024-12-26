eefea4c42ca32d3d539bfb71005d3c5f413cf147
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-12-26 17:38:55 +0900

    MARU

---
+++ Assets/Scripts/UnityC#入門-MARU-/MARU4_4_9_1.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Events;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.UIElements;
+using System.Runtime.CompilerServices;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+using System.Linq;
+using BehaviorDesigner.Runtime.Tasks.Unity.UnityPlayerPrefs;
+
+public class MARU4_4_9_1 : MonoBehaviour
+{
+	private void Start()
+	{
+		Debug.Log("スタート関数開始");
+
+		int a = 0;
+		Debug.Log($"① aの値は{a}です。");
+
+		//引数に変数aを渡し実行
+		Test(a);
+
+		Debug.Log($"③ aの値は{a}です。");
+
+		Debug.Log("スタート関数終了");
+	}
+
+	private void Test(int abc)
+	{
+		Debug.Log($"? abcの値は{abc}です。");
+		abc = 100;
+		Debug.Log($"② abcの値は{abc}です。");
+	}
+}
