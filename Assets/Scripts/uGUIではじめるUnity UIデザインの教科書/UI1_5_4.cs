f8fa3549f7e92a956bdb35fe4b3a3c262f59a391
atenai <alfamod47@yahoo.co.jp>
2025-01-26 15:10:21 +0900

    UI2

---
+++ Assets/Scripts/uGUIではじめるUnity UIデザインの教科書/UI1_5_4.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI1_5_4 : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;//回転速度を設定するフィールド
+
+    void Start()
+    {
+        //rotationSpeed = 10.0f;//回転速度を初期化
+    }
+
+    void Update()
+    {
+        //回転させる角度を算出する
+        float yAngle = rotationSpeed * Time.deltaTime;
+        //ゲームオブジェクトをY軸を基準に回転させる
+        this.transform.Rotate(0.0f, yAngle, 0.0f);
+    }
+}
