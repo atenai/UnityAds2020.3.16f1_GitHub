9d72ee0ee011c66d918ae867402b206e764028bb
atenai <alfamod47@yahoo.co.jp>
2025-01-28 23:44:50 +0900

    UI

---
+++ Assets/Scripts/uGUIではじめるUnity UIデザインの教科書/UI1_5_6_1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI1_5_6_1 : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;//フレームレートを60fpsに設定
+    }
+
+    void Update()
+    {
+
+    }
+}
