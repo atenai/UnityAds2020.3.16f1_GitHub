a9cafbdb0360f6e7cababf78058b949a330395fa
atenai <alfamod47@yahoo.co.jp>
2025-01-31 22:03:13 +0900

    プッシュテスト１

---
+++ Assets/Scripts/PushTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushTest : MonoBehaviour
+{
+    //2025/01/31
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
