0a42e014926ff0b3864acc3ac7b1761210ee3c21
atenai <alfamod47@yahoo.co.jp>
2025-07-27 21:57:58 +0900

    数独

--- Assets/Scripts/Sudoku_Udemy/Board.cs
+++ Assets/Scripts/Sudoku_Udemy/Board.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Il2Cpp;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -35,16 +36,20 @@ public class Board : MonoBehaviour
 	void Start()
 	{
 		winPanel.SetActive(false); //ゲームクリアパネルを非表示にする
-
-		difficulty = (Board.Difficulties)Settings.difficulty;
-
-		InitGrid(ref solvedGrid);
-		//DebugGrid(ref solvedGrid);
-
-		ShuffleGrid(ref solvedGrid, 5);
-		CreateRiddleGrid();
-
+		difficulty = (Board.Difficulties)Settings.difficulty;//難易度を設定
+		FillGridBase(ref solvedGrid);
+		SolveGrid(ref solvedGrid);
+		CreateRiddleGrid(ref solvedGrid, ref riddleGrid);
 		CreateButtons();
+
+		//1
+		// winPanel.SetActive(false); //ゲームクリアパネルを非表示にする
+		// difficulty = (Board.Difficulties)Settings.difficulty;//難易度を設定
+		// InitGrid(ref solvedGrid);
+		// //DebugGrid(ref solvedGrid);
+		// ShuffleGrid(ref solvedGrid, 5);
+		// CreateRiddleGrid();
+		// CreateButtons();
 	}
 
 
@@ -391,4 +396,222 @@ public class Board : MonoBehaviour
 		}
 
 	}
+
+	///BACK TRACKING
+
+	///ALL CHECKS 
+	//Columnは横
+	bool ColumnContainsNumber(int y, int value, ref int[,] grid)
+	{
+		for (int x = 0; x < 9; x++)
+		{
+			if (grid[x, y] == value)
+			{
+				return true; //もし列に同じ値があればtrueを返す
+			}
+		}
+
+		return false;
+	}
+	//Rowは縦
+	bool RowContainsNumber(int x, int value, ref int[,] grid)
+	{
+		for (int y = 0; y < 9; y++)
+		{
+			if (grid[x, y] == value)
+			{
+				return true; //もし列に同じ値があればtrueを返す
+			}
+		}
+
+		return false;
+	}
+
+	bool BlockContainsNumber(int x, int y, int value, ref int[,] grid)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (grid[x - (x % 3) + i, y - (y % 3) + j] == value)
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	bool CheckAll(int x, int y, int value, ref int[,] grid)
+	{
+		if (ColumnContainsNumber(y, value, ref grid))
+		{
+			return false;
+		}
+
+		if (RowContainsNumber(x, value, ref grid))
+		{
+			return false;
+		}
+
+		if (BlockContainsNumber(x, y, value, ref grid))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	bool IsValidGrid(ref int[,] grid)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (grid[i, j] == 0)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	///GENERATE GRID 
+	void FillGridBase(ref int[,] grid)
+	{
+		List<int> rowValues = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		List<int> colValues = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		int value = rowValues[Random.Range(0, rowValues.Count)];
+		grid[0, 0] = value;
+		rowValues.Remove(value);
+		colValues.Remove(value);
+		//ROWs FIRST
+		//1行目の値をランダムに設定
+		for (int i = 1; i < 9; i++)
+		{
+			value = rowValues[Random.Range(0, rowValues.Count)];
+			grid[i, 0] = value;
+			rowValues.Remove(value);
+		}
+
+		//COLUMNS HERE
+		for (int i = 1; i < 9; i++)
+		{
+			value = colValues[Random.Range(0, colValues.Count)];
+
+			if (i < 3)
+			{
+				while (BlockContainsNumber(0, 0, value, ref grid))
+				{
+					value = colValues[Random.Range(0, colValues.Count)];
+				}
+			}
+			grid[0, i] = value;
+			colValues.Remove(value);
+		}
+
+		DebugGrid(ref grid);
+	}
+
+	bool SolveGrid(ref int[,] grid)
+	{
+		DebugGrid(ref grid);
+
+		if (IsValidGrid(ref grid) == true)
+		{
+			return true;
+		}
+
+		//FIND FIRST FREECELL
+		int x = 0;
+		int y = 0;
+
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (grid[i, j] == 0)
+				{
+					x = i;
+					y = j;
+					Debug.Log(x + "," + y);
+					break;
+				}
+			}
+		}
+		List<int> possibilities = new List<int>();
+		possibilities = GetAllPossibilities(x, y, ref grid);
+		for (int p = 0; p < possibilities.Count; p++)
+		{
+			//SET A POSSIBLE VALUE
+			grid[x, y] = possibilities[p];
+			//BACK TRACK
+			if (SolveGrid(ref grid))
+			{
+				return true; //もし解けたらtrueを返す
+			}
+
+			grid[x, y] = 0; //もし解けなかったら、0に戻す
+		}
+
+		return false;
+	}
+
+	List<int> GetAllPossibilities(int x, int y, ref int[,] grid)
+	{
+		List<int> possibilities = new List<int>();
+		for (int val = 1; val <= 9; val++)
+		{
+			if (CheckAll(x, y, val, ref grid))
+			{
+				possibilities.Add(val);
+			}
+		}
+		return possibilities;
+	}
+
+	//NEW GAME PLAY
+	/// <summary>
+	/// ➃ここで数独の解を元に、難易度に応じて数独の問題を生成します。
+	/// 「完成された数独の解答」から、指定数だけランダムにマスを消して「問題」を作り、その内容をデバッグ表示するメソッドです。
+	/// </summary>
+	void CreateRiddleGrid(ref int[,] sGrid, ref int[,] rGrid)
+	{
+		//解答グリッドのコピー
+		//まず、solvedGrid（完成された数独の解答）をriddleGrid（問題用グリッド）にコピーします。
+		//これでriddleGridは一旦「完成された状態」になります。
+		//COPY THE SOLVED GRID
+		System.Array.Copy(sGrid, rGrid, sGrid.Length);
+
+
+		//難易度設定
+		//SET DIFFICULTY
+		SetDifficulty();
+
+		//マスを消して問題を作る
+		//piecesToErase回だけ、ランダムな位置を選び、そのマスがまだ消されていなければ（0でなければ）、そのマスを0（空欄）にします。
+		//これで「空欄のある数独の問題」ができます。
+		//ERASE FROM RIDDLE GRID
+		for (int i = 0; i < piecesToErase; i++)
+		{
+			int x1 = Random.Range(0, 9);
+			int y1 = Random.Range(0, 9);
+			//REROLL UNTIL WE FIND ONE WITHOUT A 0
+			while (rGrid[x1, y1] == 0)
+			{
+				x1 = Random.Range(0, 9);
+				y1 = Random.Range(0, 9);
+			}
+			//ONCE WE FOUND ONE WITH NO 0
+			rGrid[x1, y1] = 0;
+		}
+
+		//空白部分を0にした全てのマスの情報をデバッグログに表示します。
+		DebugGrid(ref riddleGrid);
+	}
 }
+
+
+
